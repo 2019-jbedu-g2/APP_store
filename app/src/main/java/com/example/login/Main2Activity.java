1c9ce2629e73019051fb163ba14a57f6f8a9c422
SweetRain7784 <eksql7784@naver.com>
2019-07-24 15:42:21 +0900

    first commit

---
+++ app/src/main/java/com/example/login/Main2Activity.java
@@ -0,0 +1,49 @@
+package com.example.login;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void onBackButtonClicked(View v) {
+        Toast.makeText(getApplicationContext(), "메인화면으로 돌아갑니다", Toast.LENGTH_LONG).show();
+        finish();
+
+    }
+    public void onClick(View v){
+    final EditText etEdit = new EditText(this);
+    AlertDialog.Builder dialog = new AlertDialog.Builder(Main2Activity.this);
+    dialog.setTitle("입력");
+    dialog.setView(etEdit);
+// OK 버튼 이벤트
+    dialog.setPositiveButton("예약", new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int which) {
+            String inputValue = etEdit.getText().toString();
+            Toast.makeText(Main2Activity.this,"예약되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+// Cancel 버튼 이벤트
+    dialog.setNegativeButton("취소",new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                 dialog.cancel();
+             }
+         });
+    dialog.show();
+
+
+    }
+}
---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.login;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onButton1Clicked(View v){
+        Intent intent=new Intent(getApplicationContext(),Main2Activity.class);
+        startActivity(intent);
+    }
+}
