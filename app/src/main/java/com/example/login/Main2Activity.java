fdc654e2a74b39f6c03eecf3d9c97bd0f56f1d4b
SweetRain7784 <eksql7784@naver.com>
2019-07-24 15:53:01 +0900

    test commit2

--- app/src/main/java/com/example/login/Main2Activity.java
+++ app/src/main/java/com/example/login/Main2Activity.java
@@ -11,12 +11,13 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
-
+    //test 입니다.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        //test
     }
 
     public void onBackButtonClicked(View v) {
