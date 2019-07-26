82aa62ae448915c33dbb723d6e249d9e46cbf7b6
pill328 <pill328@naver.com>
2019-07-26 17:22:21 +0900

    WebSocket connect

--- app/src/main/java/com/example/login/Main2Activity.java
+++ app/src/main/java/com/example/login/Main2Activity.java
@@ -9,19 +9,38 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.framing.Framedata;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class Main2Activity extends AppCompatActivity {
+    WebSocketClient wsc;
     Button ScannerButton;
     String barcodenumber = "";
+    String s = "";
+    TextView storeView,textView;
+    String URL = "ws://192.168.0.8:8000/queue/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         initControls();
+        Intent getin = getIntent();
+        s = getIntent().getStringExtra("result");
+        textView.setText(getIntent().getStringExtra("storeName"));
+        WebSocketConnect(s);
+        //storeView.setText(s);
         ScannerButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 IntentIntegrator intent = new IntentIntegrator(Main2Activity.this); // zxing 내부의 스캐너 호출
@@ -29,6 +48,48 @@ public class Main2Activity extends AppCompatActivity {
                 intent.initiateScan();              // 스캔화면으로 넘어감.
             }
         });
+    }
+    public void WebSocketConnect(String storeNum){
+        try{
+            Draft d = new Draft_6455();
+            StringBuffer SB = new StringBuffer();
+            SB.append(URL);
+            SB.append(storeNum);
+            SB.append("/master");
+
+            wsc = new WebSocketClient(new URI(SB.toString()),d) {
+                @Override
+                public void onOpen(ServerHandshake handshakedata) {
+                    wsc.send("");
+                }
+
+                @Override
+                public void onMessage(final String message) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            storeView.setText(message);
+                            System.out.println(message);
+                        }
+                    });
+                }
+
+                @Override
+                public void onClose(int code, String reason, boolean remote) {
+                    Toast.makeText(getApplicationContext(), "서버 연결에 종료되였습니다.", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onError(Exception ex) {
+                    ex.getStackTrace();
+                    Toast.makeText(getApplicationContext(), "서버 연결에 문제가 발생하였습니다.", Toast.LENGTH_SHORT).show();
+                }
+            };
+        }catch(URISyntaxException E){
+            E.getStackTrace();
+            Toast.makeText(getApplicationContext(), "서버 연결에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+        }
+        wsc.connect();
     }
     public void onActivityResult(int requestCode,int resultCode, Intent data){
         //  com.google.zxing.integration.android.IntentIntegrator.REQUEST_CODE
@@ -47,6 +108,12 @@ public class Main2Activity extends AppCompatActivity {
     private void initControls(){
         if (ScannerButton == null) {
             ScannerButton = (Button) findViewById(R.id.button3);
+        }
+        if (storeView == null) {
+            storeView = (TextView) findViewById(R.id.storeView);
+        }
+        if (textView == null){
+            textView = (TextView) findViewById(R.id.textView);
         }
     }
 
--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,19 +1,141 @@
 package com.example.login;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static Context mContext;
+    public static final String TAG = "MainActivity";
+    String url = "http://192.168.0.8:8000/account/";
+    String url2 = "http://192.168.0.8:8000/";
+    EditText idText = null;
+    EditText pwText = null;
+    Button loginButton;
+    ContentValues info = new ContentValues();
+    String id = "";
+    String pw = "";
+    String result, result1,result2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initControls();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                id = idText.getText().toString();
+                pw = pwText.getText().toString();
+                info.clear();
+                info.put("login",id);
+                info.put("zzzzzzzz",pw);
+                MainActivity.NetworkTask networkTask = new MainActivity.NetworkTask(url,info);
+                networkTask.execute();  // 비동기 task 작동.
+            }
+        });
     }
-    public void onButton1Clicked(View v){
-        Intent intent=new Intent(getApplicationContext(),Main2Activity.class);
-        startActivity(intent);
+
+    private void initControls() {
+        if (idText == null) {
+            idText = (EditText) findViewById(R.id.idText);
+        }
+        if (pwText == null) {
+            pwText = (EditText) findViewById(R.id.pwText);
+        }
+        if (loginButton == null) {
+            loginButton = (Button) findViewById(R.id.loginButton);
+        }
     }
+
+    public class NetworkTask extends AsyncTask<Void, Void, String> {
+
+        String url;
+        ContentValues values;
+
+        public NetworkTask(String url, ContentValues values) {
+            this.url = url;
+            this.values = values;
+        }
+
+        //background작업 시작전 ui작업을 진행.
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        // background 작업 진행
+        protected String doInBackground(Void... params) {
+            RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
+            result = requestHttpURLConnection.request(url, values); // 해당 주소로 부터 결과물 얻음
+            return result;
+        }
+
+        // 끝난후 ui진행
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            result1 = s.substring(0,9);
+            result2 = s.substring(10).trim();
+//            if(s.equals("215486211") || s.equals("289461321") || s.equals("246899512")
+//                    || s.equals("234879246") || s.equals("264875312") || s.equals("216484531")
+//                    || s.equals("246879513") || s.equals("202157945")) {
+            if(s.equals("아이디랑 비밀번호")) {
+                Toast.makeText(getApplicationContext(), "아이디와 비밀번호를 다시 확인하세요", Toast.LENGTH_LONG).show();
+            }
+            else {
+//                info.clear();
+//                info.put("queue/"+s,"master");
+//               // info.put("master","");
+//                MainActivity.NetworkTask2 networkTask2 = new MainActivity.NetworkTask2(url2,info);
+//                networkTask2.execute();
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                intent.putExtra("result",result1);
+                intent.putExtra("storeName",result2);
+                startActivity(intent);
+            }
+//            else {
+//                Toast.makeText(getApplicationContext(),"아이디와 비밀번호를 다시 확인하세요",Toast.LENGTH_LONG).show();
+//            }
+
+        }
+    }
+//
+//    public class NetworkTask2 extends AsyncTask<Void, Void, String> {
+//
+//        String url2;
+//        ContentValues values;
+//
+//        public NetworkTask2(String url2, ContentValues values) {
+//            this.url2 = url2;
+//            this.values = values;
+//        }
+//
+//        //background작업 시작전 ui작업을 진행.
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//        }
+//
+//        // background 작업 진행
+//        protected String doInBackground(Void... params) {
+//            RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
+//            result = requestHttpURLConnection.request(url2, values); // 해당 주소로 부터 결과물 얻음
+//            return result;
+//        }
+//
+//        // 끝난후 ui진행
+//        protected void onPostExecute(String s) {
+//            super.onPostExecute(s);
+//        }
+//    }
 }
